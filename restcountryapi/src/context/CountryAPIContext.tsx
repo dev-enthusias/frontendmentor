6d65db50f1b58c3f6aa617faa8074c1a5cf04b7e
Anakor Joshua <backend.mern@gmail.com>
2024-01-08 13:46:17 +0100

    Create CountryAPIContext component

---
+++ restcountryapi/src/context/CountryAPIContext.tsx
@@ -0,0 +1,28 @@
+import { Dispatch, SetStateAction, createContext, useState } from "react";
+
+import { useLoaderData } from "react-router-dom";
+
+interface APIContextProps {
+  countries: any;
+  setCountries: Dispatch<SetStateAction<any>>;
+}
+
+type ComponentProps = {
+  children: React.ReactNode;
+};
+
+const CountryAPIContext = createContext<null | APIContextProps>(null);
+
+const CountryAPIProvider = ({ children }: ComponentProps) => {
+  const countryAPIData = useLoaderData();
+
+  const [countries, setCountries] = useState(countryAPIData);
+
+  return (
+    <CountryAPIContext.Provider value={{ countries, setCountries }}>
+      {children}
+    </CountryAPIContext.Provider>
+  );
+};
+
+export { CountryAPIProvider, CountryAPIContext };
